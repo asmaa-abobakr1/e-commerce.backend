ceab0283b020acc968aa84efd4c48c86d1e1d507
asmaa-abobakr1 <asmaaahmed29699@gmail.com>
2026-06-01 16:16:02 +0300

    شس

--- middelwares/upload.middelware.js
+++ middelwares/upload.middelware.js
@@ -1,10 +1,11 @@
 const multer = require('multer');
 const path = require('path');
+const os = require('os');
 const AppError = require('../utilites/appError.uti');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, os.tmpdir());
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
